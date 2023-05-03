ca094fd0d88e07f2c868983455d404e2d6cebddf
gary <gary.kindel@gmail.com>
2023-05-03 19:07:17 -0400

    Set background to be gray

--- CameraInspector/CameraInspector/App.xaml.cs
+++ CameraInspector/CameraInspector/App.xaml.cs
@@ -13,5 +13,12 @@ namespace CameraInspector
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Set the ShutdownMode property to OnLastWindowClose.
+            ShutdownMode = ShutdownMode.OnLastWindowClose;
+        }
     }
 }
